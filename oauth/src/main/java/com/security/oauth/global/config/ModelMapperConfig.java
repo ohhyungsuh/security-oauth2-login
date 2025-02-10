c1d59d873238cf6a70cbcc84c1441371b257bea7
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-02-10 23:58:46 +0900

    feat: ModelMapper 빈 등록

---
+++ oauth/src/main/java/com/security/oauth/global/config/ModelMapperConfig.java
@@ -0,0 +1,20 @@
+package com.security.oauth.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setFieldMatchingEnabled(true)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+}
